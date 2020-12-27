ebc9cf60e0d0d92dfda56c335dd7db188b80afa0
Glory Adedayo <53071086+glory9@users.noreply.github.com>
2020-12-27 16:29:42 -0600

    Update matcher.py

--- matcher.py
+++ matcher.py
@@ -12,35 +12,38 @@ def getWordsList(char):
 
 class Matcher:
     def __init__(self, char):
-        self.words = getWordsList(char)
+        self.potential_matches = getWordsList(char)
 
         # for every processed sequence for any given character, we need
-        # self.words to hold a complete list of all potential matching words.
-        # but since self.words gets altered during each sequence, we can use a 
-        # cache to quickly obtain the original list of matching words
+        # self.potential_matches to hold a complete list of all potentially 
+        # matching words. But since self.potential_matches gets altered after
+        # processing each sequence/direction, we can use a cache to quickly 
+        # re-compute the original list of matching words.
         self.cache = set(self.words)
 
     def process_sequence(self, letters):
         curr_idx = 0
         correct_matches = []
 
-        while curr_idx < len(letters):
+        while curr_idx < len(letters) and len(self.potential_matches) > 0:
             match_char = letters[curr_idx]
-            # we need to delete some items during the iteration, so we keep
-            # a copy of self.words instead, then delete the respective items
-            # from the copy and finally update self.words after the iteration
-            matching_words = set(self.words)
+            # we need to delete some items during the iteration, so we keep a copy
+            # of self.potential_matches instead, then delete the respective items
+            # from the copy and finally update self.words after the iteration.
+            updated_potential_matches = set(self.potential_matches)
 
-            for word in self.words:
+            for word in self.potential_matches:
                 if word[curr_idx] != match_char:
-                    matching_words.remove(word)
+                    updated_potential_matches.remove(word)
                 elif curr_idx == len(word) - 1:
-                    correct_matches.append(word)
-                    matching_words.remove(word)
+                    # we only add words of length 3 or more (i.e, curr_idx 2 and above)
+                    if curr_idx > 1:
+                        correct_matches.append(word)
+                    updated_potential_matches.remove(word)
 
-            self.words = matching_words
+            self.potential_matches = updated_potential_matches
             curr_idx += 1
         # update self.words to its original state after processing each sequence
-        self.words = set(self.cache)
+        self.potential_matches = set(self.cache)
 
         return correct_matches
